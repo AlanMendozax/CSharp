4b8870ef80b5857f2917ac12dc3ee112d63b5955
Alan Mendoza <alanmendoza9801@outlook.com>
2021-08-18 17:44:57 -0500

    Add files via upload

---
+++ Exercises/ConversionesInfijo/ConversionesInfijo/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConversionesInfijo
+{
+    public partial class Form1 : Form
+    {
+        Stack pila = new Stack();
+        char dato;
+        int tope = -1, max=0;
+        Boolean res;       
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+          
+
+        } 
+        PilaAA(int max)
+        {
+            this.max = max;
+            pila= new Stack[max];
+            dato = '0';
+        }
+        public void borrarPila()
+        {
+            tope = -1;
+        }
+        public Boolean llena()
+        {
+            if (tope == (max -1))
+            {
+                res = true;
+            }
+            else
+            {
+                res = false;
+            }
+            return res;
+        }
+        public Boolean vacia()
+        {
+            if(tope == -1)
+            {
+                res = true;
+            }
+            else
+            {
+                res = false;
+            }
+            return res;
+        }
+        public Boolean push(char dato)
+        {
+            if (llena())
+            {
+                MessageBox.Show("Error: Pila llena");
+            }
+            else
+            {
+                tope++;
+                pila[tope] = dato;
+                res = true;
+            }
+            return res;
+        }
+        public char pop()
+        {
+            if (vacia())
+            {
+                MessageBox.Show("Sub-desbordamiento: Pila vacia");
+            }
+            else
+            {
+                dato = pila[tope];
+                tope--;
+            }
+            return dato;
+        }
+
+        public char GetTope()
+        {
+            char top = '0';
+            if (vacia())
+            {
+
+            }
+            else
+            {
+                top = pila[tope];
+                return top;
+            }            
+        }
+    }
+}
---
+++ Exercises/DataGridView/DataGrithView/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DataGrithView
+{
+    public partial class Form1 : Form
+    {
+        DataTable directorio = new DataTable();
+        public Form1()
+        {
+            InitializeComponent();
+            directorio.Columns.Add("Contacto");
+            directorio.Columns.Add("Número");
+            directorio.Columns.Add("E-mail");
+            directorio.Rows.Add(new object[] { "Jose", "1(234)567-8910", "easd@hotmail.com" });
+            directorio.Rows.Add(new object[] { "Miriam", "1(234)567-8910", "easd@hotmail.com" });
+            directorio.Rows.Add(new object[] { "Josélito", "1(234)567-8910", "easd@hotmail.com" });
+            Tablita.DataSource = directorio;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Tablita.Columns.Add("nombre","Nombre");
+            //Tablita.Columns.Add("apellido", "Apellido");
+            //Tablita.Columns.Add("telefono", "Telefono");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            directorio.Rows.Add(textBox1.Text,textBox2.Text,textBox3.Text);
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+            textBox1.Focus();
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+            directorio.DefaultView.RowFilter = $"Contacto LIKE '{textBox4.Text}%'";
+        }
+    }
+}
