88eb27b3e7c4ea74e4d3fe11bf74b0d386ab83d6
Alan Mendoza <alanmendoza9801@outlook.com>
2021-08-18 17:51:39 -0500

    Add files via upload

---
+++ Exercises/Combo y textbox/Combo y textbox/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MiClase;
+
+namespace Combo_y_textbox
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Class1 x = new Class1();
+            comboBox1.Items.Add(x.ybt(textBox1.Text));
+            textBox1.Focus();
+            textBox1.Clear();
+        }
+    }
+}
